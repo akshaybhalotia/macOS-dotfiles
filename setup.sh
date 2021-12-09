0a2c84090385def3129399783feda0b5f1195366
akshaybhalotia <github@axy.one>
2021-12-10 01:44:57 +0530

    fix: update script

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ brew bundle
 sudo xcodebuild -license accept
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
@@ -31,7 +32,7 @@ nvm install node
 
 sh vscode_extensions.sh
 
-export MACPREFS_BACKUP_DIR="$HOME/code/macOS-dotfiles"
+export MACPREFS_BACKUP_DIR="$HOME/code/macOS-dotfiles/macprefs"
 sudo macprefs restore
 
 touch ~/.mackup.cfg
