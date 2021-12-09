e1883a128d974d537c81ab89cbf0f817a0a66e51
akshaybhalotia <github@axy.one>
2021-12-09 21:28:08 +0530

    fix: update script

--- setup.sh
+++ setup.sh
@@ -5,21 +5,29 @@ chsh -s /bin/zsh
 sudo softwareupdate -i -a
 xcode-select --install
 
-nvram StartupMute=%00
+sudo nvram StartupMute=%00
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 src
 nvm install node
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle
-
 sh vscode_extensions.sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export MACPREFS_BACKUP_DIR="$HOME/code/macOS-dotfiles"
+sudo macprefs restore
 
-macprefs restore
-mackup restore
+touch ~/.mackup.cfg
+echo "[storage]
+engine = file_system
+path = code/macOS-dotfiles" >> ~/.mackup.cfg
+mackup restore --force
